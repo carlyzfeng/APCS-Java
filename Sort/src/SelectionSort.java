d83b61ded1b0c37fc7f40f963c55dbb2fba2f981
Carly Feng <carlyzfeng@gmail.com>
2018-07-18 17:14:21 -0700

    Create SelectionSort.java

---
+++ Sort/src/SelectionSort.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class SelectionSort {
+//NEED TO FINISH
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] data = new int[10];
+		data[0] = 4;
+		data[1] = 6;
+		data[2] = 3;
+		data[3] = 8;
+		data[4] = 9;
+		data[5] = 2;
+		data[6] = 1;
+		data[7] = 5;
+		data[8] = 7;
+		data[9] = 0;
+		for (int w = 0; w < data.length; w++) {
+			for (int i = w; i > 0; i--) {
+				if(data[i] < data[i-1]) {
+					int x = data[i];
+					data[i] = data[i-1];
+					data[i-1] = x;
+				}
+			}
+		}
+		System.out.println(Arrays.toString(data));
+	}
+	
+}
