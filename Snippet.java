71e9d76d0de91634009b391047ebc1217aec228e
Carly Feng <carlyzfeng@gmail.com>
2018-07-18 17:50:20 -0700

    Add files via upload

---
+++ Snippet.java
@@ -0,0 +1,15 @@
+package snippet;
+
+public class Snippet {
+	int x;
+	Randp rand = new Randp(6);
+	x = rand.nextInt();	// randomly returns 5
+	x = rand.nextInt();	// randomly returns 2
+	x = rand.nextInt();	// randomly returns 6
+	x = rand.nextInt();	// randomly returns 4
+	x = rand.nextInt();	// randomly returns 1
+	x = rand.nextInt();	// randomly returns 3
+	x = rand.nextInt();	// returns 0
+	x = rand.nextInt();	// returns 
+}
+
